3aa05e56752ba9c856ae0bd78c81345e4d62ddd4
itamar.oliveira <itamar.oliveira@globalhitss.com.br>
2024-04-05 15:30:21 -0300

    subindo por etapa

---
+++ TarefasAppBlazor.UI3/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TarefasAppBlazor.UI3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
